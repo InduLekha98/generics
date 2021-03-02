3292f39ebf7eb513e7fce52351e2f5776b3982ca
InduLekha98 <rindulekha09@gmail.com>
2021-03-03 00:28:21 +0530

    Addeed:Extending max method using generic method

---
+++ TestMaximumTestCase.java
@@ -0,0 +1,53 @@
+package com.TestMaximum;
+
+public class TestMaximumTestCase {
+    public static void main(String[] args)
+    {
+        Integer[] integerArr= {1,2,3,4,5};
+        Float[] floatArr= {10f,20f,30f,40f,50f};
+        String[] stringArr= {"Apple","Peach","Banana","Mango","strawberry"};
+        printArr(integerArr);
+        printArr(floatArr);
+        printArr(stringArr);
+        maxFind(integerArr);
+        maxFind(floatArr);
+        findmaxString(stringArr);
+
+
+
+
+    }
+
+    public static<E>void printArr(E[] element)
+    {
+        for(E i : element )
+            System.out.print(i+" ");
+        System.out.println();
+    }
+
+    public static  <T extends  Comparable <T>> void maxFind(T[] element)
+    {
+        T max=element[0];
+        for(T i:element)
+        {
+            if(i.compareTo(max)> 0)
+            {
+                max=i;
+            }
+        }System.out.println(max+" ");
+    }
+
+    public static <T extends Comparable<T>> void findmaxString(T[] elements) {
+        T max = elements[0];
+        for (T i : elements)
+        {
+            if (Integer.valueOf(i.toString().length()).compareTo(Integer.valueOf(max.toString().length())) > 0)
+            {
+                max = i;
+            }
+        }
+        System.out.println("the maximum value in array is " + max);
+    }
+
+
+}
